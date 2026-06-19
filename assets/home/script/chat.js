896ed907b936548d982963a7b9fabadbfe41bedd
Divya <murugesanmdivya@gmail.com>
2026-06-19 23:08:29 +0530

    Updated index.html, image and script

---
+++ assets/home/script/chat.js
@@ -0,0 +1,12 @@
+ (function(d, w, c) {
+        w.BrevoConversationsID = '65d2dad099d9ec596d73726c';
+        w[c] = w[c] || function() {
+            (w[c].q = w[c].q || []).push(arguments);
+        };
+        var s = d.createElement('script');
+        s.async = true;
+        s.src = 'https://conversations-widget.brevo.com/brevo-conversations.js';
+        if (d.head) d.head.appendChild(s);
+
+      
+    })(document, window, 'BrevoConversations');
